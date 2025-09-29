f49fa818f79d9c1fb48dda6fe9ca5e43d02e630d
Petr Pivoňka <petrpiv1@gmail.com>
2025-09-29 23:14:59 +0200

    Increase length in migration

---
+++ Revalidate/Migrations/20250929211356_FixInputsResultLength.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Revalidate.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixInputsResultLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InputsResult",
+                table: "ValidationDistroResults",
+                type: "longtext",
+                maxLength: 32767,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1024)",
+                oldMaxLength: 1024,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InputsResult",
+                table: "ValidationDistroResults",
+                type: "varchar(1024)",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldMaxLength: 32767,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
