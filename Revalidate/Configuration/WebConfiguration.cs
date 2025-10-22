1f9ddc4317d491c4d136ec8fe5a738e564c80023
Petr Pivoňka <petrpiv1@gmail.com>
2025-10-23 00:35:33 +0200

    Fix forwarded headers

--- Revalidate/Configuration/WebConfiguration.cs
+++ Revalidate/Configuration/WebConfiguration.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Revalidate.Api;
 using Revalidate.Api.Converters.Json;
 using Revalidate.Services;
+using System.Net;
 using System.Text.Json.Serialization;
 using System.Threading.RateLimiting;
 
@@ -117,13 +118,25 @@ public static class WebConfiguration
             options.Providers.Add<GzipCompressionProvider>();
         });
 
+        // Figures out HTTPS behind proxies
         services.Configure<ForwardedHeadersOptions>(options =>
         {
             options.ForwardedHeaders =
-                ForwardedHeaders.XForwardedFor |
-                ForwardedHeaders.XForwardedProto;
-            options.KnownIPNetworks.Clear();
-            options.KnownProxies.Clear();
+                ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+
+            foreach (var knownProxy in config.GetSection("KnownProxies").Get<string[]>() ?? [])
+            {
+                if (IPAddress.TryParse(knownProxy, out var ipAddress))
+                {
+                    options.KnownProxies.Add(ipAddress);
+                    continue;
+                }
+
+                foreach (var hostIpAddress in Dns.GetHostAddresses(knownProxy))
+                {
+                    options.KnownProxies.Add(hostIpAddress);
+                }
+            }
         });
     }
 }
